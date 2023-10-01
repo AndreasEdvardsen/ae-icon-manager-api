b29de4398f9f92220e45c7f9b05e5c23ff88594a
Andreas Edvardsen <edvardsenandreas@outlook.com>
2023-10-01 16:30:10 +0200

    changed mongodb client and added filters to icon endpoint

--- app.js
+++ app.js
@@ -2,44 +2,48 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 
-const Realm = require("realm-web");
-const realmApp = new Realm.App({ id: "data-lskup" });
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.MONGODB_URI;
 
-var icons = [];
+const client = new MongoClient(uri);
 
 app.get("/icons", (req, res) => {
-  res.send(icons);
+  const prefix = req.query.prefix;
+  if (prefix) {
+    getIconsByPrefix(prefix).then((icons) => {
+      res.send(icons);
+    });
+  } else {
+    res.status(400).send("Missing prefix query parameter");
+  }
+});
+
+app.get("/distinctIconPrefixes", (req, res) => {
+  getDistinctIconPrefixes().then((prefixes) => {
+    res.send(prefixes);
+  });
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-async function loginEmailPassword(email, password) {
-  // Create an email/password credential
-  const credentials = Realm.Credentials.emailPassword(email, password);
-  // Authenticate the user
-  const user = await realmApp.logIn(credentials);
-  // 'App.currentUser' updates to match the logged in user
-  console.assert(user.id === realmApp.currentUser.id);
-  return user;
+async function getDistinctIconPrefixes() {
+  const iconsCollection = client.db("AEIconManager").collection("Icons");
+  return await iconsCollection.distinct("prefix");
 }
 
-loginEmailPassword(process.env.USER_ID, process.env.USER_KEY).then((user) => {
-  const mongo = realmApp.currentUser.mongoClient("AEProjects");
-  const iconsCollection = mongo.db("AEIconManager").collection("Icons");
+async function getIconsByPrefix(prefix) {
+  const iconsCollection = client.db("AEIconManager").collection("Icons");
+  var icons = await iconsCollection.find({ prefix: prefix });
 
-  iconsCollection
-    .find({
-      prefix: "mdi",
-    })
-    .then((results) => {
-      results.forEach((icon) => {
-        var image = encodeURIComponent(
-          icon.image.replace("<path ", `<path style="fill:#FFFFFF" `)
-        );
-        icon.imagePath = `data:image/svg+xml,${image}`;
-        icons.push(icon);
-      });
-    });
-});
+  var manipulatedIcons = [];
+  for await (const icon of icons) {
+    var image = encodeURIComponent(
+      icon.image.replace("<path ", `<path style="fill:#FFFFFF" `)
+    );
+    icon.imagePath = `data:image/svg+xml,${image}`;
+    manipulatedIcons.push(icon);
+  }
+  return manipulatedIcons;
+}
