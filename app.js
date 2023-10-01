447335904b2e12b0aef1a6ddeaf38481ba7d8a5e
Andreas Edvardsen <edvardsenandreas@outlook.com>
2023-10-01 09:50:35 +0200

    fix origin again and remove console.log

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(function (req, res, next) {
   // Website you wish to allow to connect
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://seashell-app-wwgas.ondigitalocean.app/"
+    "https://andreasedvardsen.github.io/ae-icon-manager/"
   );
 
   // Request methods you wish to allow
